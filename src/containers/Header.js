af184bff06c19596e08cd8635a17f8ccf8c65921
Yathiraj <yathirajs@idsinternal.com>
2022-02-14 10:10:15 +0000

    changes of header

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -18,8 +18,8 @@ const Header =()=>{
         <li style={{padding:14, fontSize:18}}><Link to="/checkout"> Cart({cart && cart.length}) </Link></li>
         <li  style={{padding:14, fontSize:18}}>
         <Link to={`${user ? "/" : "/login"}`} >
-        <div onClick={authHandle}>
-        <h3>Hii,&nbsp;<span>{user ? user.email: "Guest"}{""}</span>&nbsp;&nbsp;<span>{user ? "Sign Out" : "Sign In"}</span></h3>
+        <div>
+        <h3>Hii,&nbsp;<span>{user ? user.email: "Guest"}{""}</span>&nbsp;&nbsp;<span onClick={authHandle}>{user ? "Sign Out" : "Sign In"}</span></h3>
         </div>
         </Link></li>
         </ul> </div>
